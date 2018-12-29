d5619ef6cad313f51c55910b84800f2433e38ea6
DESKTOP-HHU024D\roeey <roeeyg@gmail.com>
2018-12-29 18:09:36 +0200

    Change Themes configuration

--- src/AppProperties/Themes.java
+++ src/AppProperties/Themes.java
@@ -22,45 +22,45 @@ public enum Themes {
     public String getRegularPipe() {
         switch (this) {
             case Blue:
-                return "./resources/TubeCorner-R02.png";
+                return "./resources/Blue.png";
             default:
-                return "./resources/Tube-R01.png";
+                return "./resources/Gray.png";
         }
     }
 
     public String getAnglePipe() {
         switch (this) {
             case Blue:
-                return "./resources/TubeCorner-R02.png";
+                return "./resources/BlueCorner.png";
             default:
-                return "./resources/TubeCorner-R01.png";
+                return "./resources/GrayCorner.png";
         }
     }
 
     public String getBackgroundImage() {
         switch (this) {
             case Blue:
-                return "./resources/Wall-R01.jpg";
+                return "./resources/BlueBackGround.jpg";
             default:
-                return "./resources/Wall-R02.jpg";
+                return "./resources/GrayBackGround.jpg";
         }
     }
 
     public String getEndImage() {
         switch (this) {
             case Blue:
-                return "./resources/End-R01A.png";
+                return "./resources/BLueSE.png";
             default:
-                return "./resources/End-R01.png";
+                return "./resources/GraySE.png";
         }
     }
 
     public String getStartImage() {
         switch (this) {
             case Blue:
-                return "./resources/End-R01A.png";
+                return "./resources/BLueSE.png";
             default:
-                return "./resources/Start-R01.png";
+                return "./resources/GraySE.png";
         }
     }
 }
